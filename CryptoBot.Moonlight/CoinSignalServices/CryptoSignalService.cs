58749e06ac3a4a46f694b9aecd2d65b62c197b6d
defneaynur <defneaynur.gkgz@gmail.com>
2024-10-18 10:36:28 +0300

    Add project files.

---
+++ CryptoBot.Moonlight/CoinSignalServices/CryptoSignalService.cs
@@ -0,0 +1,29 @@
+﻿using Binance.Net.Clients;
+using CryptoExchange.Net.Authentication;
+
+public static class CryptoSignalService
+{
+    public static async Task<decimal> GetBitcoinPrice()
+    {
+        var apiCredentials = new ApiCredentials("vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A",
+                                                "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j");
+
+
+        using var binanceClient = new BinanceRestClient(options =>
+        {
+            options.ApiCredentials = apiCredentials;
+        });
+
+        var result = await binanceClient.SpotApi.ExchangeData.GetPriceAsync("BTCUSDT");
+
+
+        if (result.Success)
+        {
+            return result.Data.Price;
+        }
+        else
+        {
+            throw new Exception($"API Error: {result.Error.Message}");
+        }
+    }
+}
---
+++ CryptoBot.Moonlight/Program.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+
+var botClient = new TelegramBotClient("8030439322:AAELZT29CNxHadtjejDx6G1yei1gWKrNf6U");
+var price =await  CryptoSignalService.GetBitcoinPrice();
+
+// Mesaj göndereceğiniz kanalın ID'sini girin (örneğin @kanal_adı veya kanalın numeric ID'si)
+var chatId = "@Test_MoonChannel";
+var messageText = "Bu bir kripto sinyali! BTC şu an " + price.ToString();
+
+await botClient.SendTextMessageAsync(
+    chatId: chatId,
+    text: messageText
+);
+
+Console.WriteLine("Mesaj gönderildi.");
